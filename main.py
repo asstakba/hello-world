b39ccb782295c634b2fd7c2e386c9da2e842d23a
Aissata Kaba <e6223450@cmaisonneuve.qc.ca>
2024-05-11 16:14:29 -0400

    Ajout de phrase justifier

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #extrait les mots du fichiers
-
-'''with open("prenoms.txt", "r") as f:
-    print(f.read())'''
-
+'''
+with open("aliments.txt", "r") as f:
+    print(f.read())
+'''
 #extrait les mots du fichiers en liste
 '''
 with open("prenoms.txt", "r") as f:
@@ -39,6 +39,30 @@ with open("prenoms.txt", "r") as f:
         print(ligne.replace('\n', ''))
    
 '''
-with open("prenoms.txt", "r", encoding="utf-8") as f:
+'''with open("aliments.txt", "r", encoding="utf-8") as f:
     for ligne in f:
         print(ligne.replace('\n', ''))
+'''
+# Fonction pour lire un fichier
+# Retourne le contenu du fichier
+def read_file(path):
+    chaine = ""
+    with open(path, 'r', encoding = 'utf-8') as f:
+        chaine = f.read()
+    return chaine
+
+
+# Fonction pour écrire le contenu dans un fichier.
+def write_file(path, contenu):
+    with open(path, 'w', encoding ='utf-8') as f:
+        f.write(contenu)
+
+# Fonction qui fait la lecture, la justification, puis l'écriture.
+# Aidez-vous de vos fonctions read_file et write_file.
+def justifier(path):
+    chaine = read_file(path)
+    chaine2 = chaine.replace('\n', ' ')
+    chaine3 = chaine2.replace('. ', '.\n')
+    write_file('phrasesjustier.txt', chaine3)
+
+justifier('phrasesjustier.txt')
