67dfa3f7133df8b19953aef4d1ac84cfb50f5b7d
Aissata Kaba <e6223450@cmaisonneuve.qc.ca>
2024-05-11 13:35:50 -0400

    Ajout des textes

--- main.py
+++ main.py
@@ -1,4 +1,44 @@
-def test(x,y):
-    print(x+y)
+#extrait les mots du fichiers
 
-test(10, 20)
+'''with open("prenoms.txt", "r") as f:
+    print(f.read())'''
+
+#extrait les mots du fichiers en liste
+'''
+with open("prenoms.txt", "r") as f:
+    print(f.readlines())
+'''
+
+'''with open("prenoms.txt", "r") as f:
+    lines = f.readlines()
+    lines = [line.strip() for line in lines]
+    print(lines)
+'''
+"""with open("prenoms.txt", "r") as f:
+    '''while True:
+        s = f.readline()
+        if s != "":
+            print(s)
+        else:
+            break
+        '''
+    while True:
+        s = f.readline()
+        if s != "":
+            print(s.strip())
+        else:
+            break
+"""
+'''
+with open("prenoms.txt", "r") as f:
+    for ligne in f:
+        print(ligne)'''
+
+'''with open("prenoms.txt", "r") as f:
+    for ligne in f:
+        print(ligne.replace('\n', ''))
+   
+'''
+with open("prenoms.txt", "r", encoding="utf-8") as f:
+    for ligne in f:
+        print(ligne.replace('\n', ''))
